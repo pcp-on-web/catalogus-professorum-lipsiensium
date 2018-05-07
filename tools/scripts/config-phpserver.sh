5b4558b540662c8fe62fe3232ab57d195a68835b
triechert <thomas.riechert@htwk-leipzig.de>
2018-05-07 23:52:53 +0200

    script fixes

--- tools/scripts/config-phpserver.sh
+++ tools/scripts/config-phpserver.sh
@@ -6,7 +6,6 @@ echo "---"
 
 echo "Change Passwords"
 echo "---"
-dba_pw="$( cat pw | cut -d " " -f 4 )"
-
+dba_pw="$( cat /import-data/pw | cut -d " " -f 4 )"
 sed s/store\.virtuoso\.password\ \ \ \ \=\ \"dba\"/store\.virtuoso\.password\ \ \ \ \=\ \"$dba_pw\"/g -i /var/www/html/config.ini
 
--- tools/scripts/config-virtuoso.sh
+++ tools/scripts/config-virtuoso.sh
@@ -5,10 +5,9 @@ echo "---"
 
 echo "Change Passwords"
 echo "---"
-isql-v 1111 dba dba pw
-dba_pw="$( cat pw | cut -d " " -f 4 )"
+isql-v 1111 dba dba /import-data/pw
+dba_pw="$( cat /import-data/pw | cut -d " " -f 4 )"
 
 echo "Import knowledgebases from /import-data/"
 echo "---"
-isql-v 1111 dba dba pw
-dba_pw="$( cat pw | cut -d " " -f 4 )"
+
