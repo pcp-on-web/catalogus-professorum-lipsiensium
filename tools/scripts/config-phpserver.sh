b569a0df3d340a96c9223d7c6f6a2a3e4a7e911a
triechert <thomas.riechert@htwk-leipzig.de>
2018-05-07 23:31:56 +0200

    last line removed

--- tools/scripts/config-phpserver.sh
+++ tools/scripts/config-phpserver.sh
@@ -9,4 +9,4 @@ echo "---"
 dba_pw="$( cat pw | cut -d " " -f 4 )"
 
 sed s/store\.virtuoso\.password\ \ \ \ \=\ \"dba\"/store\.virtuoso\.password\ \ \ \ \=\ \"$dba_pw\"/g -i /var/www/html/config.ini
-root@545d1a4f6c3f:/scripts# more /var/www/html/config.ini
+
