9aa9112da2ac26cb4df5e56abd52d3811c6f8b70
triechert <thomas.riechert@htwk-leipzig.de>
2018-05-11 14:26:45 +0200

    cpl theme

--- customize/customize.sh
+++ customize/customize.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # copy Theme
-cp -R /customize/theme/heloise /var/www/extensions/themes/
-ln -s /var/www/extensions/themes/silverblue/sandbox /var/www/extensions/themes/heloise/sandbox
-ln -s /var/www/extensions/themes/silverblue/scripts /var/www/extensions/themes/heloise/scripts
+cp -R /customize/theme/cpl /var/www/html/extensions/themes/
+ln -s /var/www/html/extensions/themes/silverblue/sandbox /var/www/html/extensions/themes/cpl/sandbox
+ln -s /var/www/extensions/htm/themes/silverblue/scripts /var/www/html/extensions/themes/cpl/scripts
 
-echo 'themes.default = "heloise"' >> /var/www/config.ini
+echo 'themes.default = "cpl"' >> /var/www/html/config.ini
 
 # set  extension
-cp /customize/extensions/*.ini /var/www/extensions/
+cp /customize/extensions/*.ini /var/www/html/extensions/
 
 echo 'properties[] = "http://catalogus-professorum.org/cpm/2/picture"' >> /var/www/html/extensions/imagelink/default.ini 
 
